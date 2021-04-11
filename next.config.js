c7a33fd524a70822eaac6311aea8df155d78dc3c
Krychaxp <krychaxp@op.pl>
2021-04-11 23:00:11 +0200

    add material-ui to ssr

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-require('dotenv').config()
 const nextTranslate = require("next-translate");
 const { withGoogleFonts } = require("nextjs-google-fonts")
 
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,32 +1,32 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+import { ServerStyleSheets } from "@material-ui/core/styles";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
+    const sheets = new ServerStyleSheets();
+
     const originalRenderPage = ctx.renderPage;
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => [
+          sheet.collectStyles(<App {...props} />),
+          sheets.collect(<App {...props} />),
+        ],
+      });
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: [
+        initialProps.styles,
+        sheet.getStyleElement(),
+        sheets.getStyleElement(),
+      ],
+    };
   }
+
   render() {
     return (
       <Html>
